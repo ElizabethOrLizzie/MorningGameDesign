43024f58352219a0ad084fbd0a2ed8255d99aa22
ElizabethOrLizzie <nassie25@greenhill.org>
2022-06-01 09:38:34 -0500

    new one

---
+++ Add.py
@@ -0,0 +1,15 @@
+#Elizabeth Nassi
+#Twonumbers
+# We will ask user for two numbers
+# and add them
+#anything after a # is
+#ignored by the computer
+#import things
+import os
+os.system('cls')
+#Use Variables
+Num1=23
+Num2=12
+Answer=Num1+Num2
+# Operators +,-,*,/
+print('The addition of ',Num1, 'and ',Num2, 'is ',Answer,)
