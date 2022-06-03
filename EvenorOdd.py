1a36820a2e82209bc3c040a581b93088ac1f2e4b
ElizabethOrLizzie <nassie25@greenhill.org>
2022-06-03 09:06:41 -0500

    Corrected version of homework

--- EvenorOdd.py
+++ EvenorOdd.py
@@ -2,4 +2,12 @@
 #multiple of three or five
 import os
 os.system("cls")
-Number=input('Enter a number: ')
+Number=int(input('Enter a number: '))
+if (Number%2==0):
+    print ('This is an even number.')
+else:
+    print ('This is an odd number.')
+if (Number%3==0):
+    print ('This is a multiple of three.')
+if (Number%5==0):
+    print ('This is a multiple of five.')
