cc878b45df9a960fcb8104ea518894fb3938ab91
ElizabethOrLizzie <nassie25@greenhill.org>
2022-06-06 20:14:58 -0500

    yay

---
+++ Fernandolist.py
@@ -0,0 +1,111 @@
+# Fernando Gupta
+# create a word guess agme with 10 words
+# pseudocode: get one word from a list of animals provide instructions to user, one hint about the words in the list and the amount of words in the list, get guess. If correct congratulate. if not say you missed. if missed provide another hint. if correct congatulate if inncorect say, man wrong again and then provide third hint, if correct congatulate, if innocorect say you missed again, you are really bad at this, stop providing hits and give user unlimited guesses till correct
+#  
+#give a variable for loops, she doesnt like breaks, seems "lazy"
+#game=true
+#cnt=0
+#a function is a section of the program we call when we need it
+#def hint()
+#global cnt #will allow us to modify the variable all over the program
+#if cnt=0
+#first hint
+#cnt+=1 #cnt=cnt+1
+#if cnt==1
+#while game
+#name= input("what is your name? ") #name make personalized, also will help keep score
+#print(name, end=",")
+#answer=input("would you like to play? ")
+#if "n" or "N" or in answer:
+#break #Other cases can put game= False, because loop will finish before it breaks
+
+#os.system ("cls") makes clear slate
+
+#trial and error, keep from user put wrong answer and crash program #prevent error try and except
+#while True:
+#try:
+#choice=int(choice)
+#if choice>0 and choice<4
+#else:
+# print("give me 1, 2, or 3,")
+#break
+#except:
+#print("sorry, not valid answer")
+
+from ctypes.wintypes import WORD
+import random
+import os 
+
+os.system ('cls')
+from time import sleep
+seconds=.5
+
+
+
+list = ["coral","scallop","sea urchin","oyster","mussel","cockle","clam","geoduck","abelone","ostrea"]
+theword=random.choice(list)
+Game=True 
+cnt=0
+# a function is a section of the program that we call when we need it 
+def hint():
+    print("|*************************************|")
+    print("|         Here is a new hint          |")
+    print("|These creatures all have a hard shell|")
+    print("|        only 2 shells in fact        |")
+    print("|*************************************|")
+
+    guess2 = input("plese put your new guess here: ")
+    if guess2 == theword:
+        print("Congrats, You got it")
+    else:
+        print("wrong again, you are pretty bad at this, try again")
+
+    print("|**************************************|")
+    print("|       Here is your final hint        |")
+    print("|  These creatures almost never move   |")
+    print("|**************************************|")
+
+while Game:
+    print("|***************************************|")
+    print("|         Guess The Animal!!!           |")
+    print("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|")
+    print("|          1.Instructions               |")
+    print("|   There are 10 animals in this game   |")
+    print("|     You must guess one of them        |")
+    print("|First we will provide you with one hint|")
+    print("|           !Your Hint Is!              |")
+    print("|  These animals are big fans of water  |")
+    print("|***************************************|")
+# user name make more persoal and will be used to keep score    
+    name= input('what is your name: ')
+    print(name, end=" ")
+    answer=input("would you like to  play " )
+    if 'n' or 'N' in answer:
+        break
+
+    guess1=input("plese put your guess here: ")
+    if guess1 == theword:
+        print("Congrats, You got it")
+    else:
+        print("you missed it, try again")
+
+
+
+    guess3 = input("plese put your final guess here: ")
+
+    if guess3 == theword:
+        print("Congrats, You got it")
+    else:
+        print("You are horrible at guessing, no more hints, go till you get it right")
+
+
+    # Give a Variable to your loop 
+    check=True 
+    while check:
+        ans = input("plese put your guess here: ")
+        if ans == theword:
+            name = True 
+            print("Congrats, You got it")
+            check=False 
+        else:
+            print("wrong again, try again")
--- Listwordguess.py
+++ Listwordguess.py
@@ -2,32 +2,213 @@
 #have user guess random flower from list
 #pseudocode
 #Make list of different flowers, Choose random flower off of list, Print instructions of game, Have user guess flower, If guess is correct, print congratulations, If guess is incorrect, print that they guessed incorrectly, Ask if still want to guess, If yes, loop, If no, give answer, Ask user if want to keep playing, If yes, Choose new random flower, loop, If no, Say bye, end
-
+#make three different list game options, not just flower
 import random
 import os
+#from traceback import print_stack
 os.system("cls")
-print('*🌸🌹🌺🌻Guess the flower!!!🌼🌷🥀💐*')
-print("**Make sure that your answers aren't capitalized!**")
-list = ["tulip", "lilac", "sunflower", "daisy", "hibiscus", "rose", "hydrangea", "lily", "iris", "hyacinth", "daffodil", "lavendar", "poppy", "peony"]
-flower = random.choice(list)
-while True:
-    guess = input("Your guess is...? 🤔 ")
-    if guess in flower:
-        print("🌼🌷🥀💐 CONGRATULATIONS!!! You guessed correctly. 😀 🌸🌹🌺🌻")
-        play = input("Want to play again? 💐 ")
-        if play == "no":
-            print("ok bye 👋")
-            break
-        if play == "yes":
-           flower = random.choice(list)
-    else:
-        print("Sorry, that's not correct. 🙁")
-        ans = input("Want to guess again? Answer yes or no. ")
-        if ans == "no":
-            print ("The flower was ", flower)
-            play = input("Want to play again? 💐 Answer yes or no. ")
-            if play == "no":
-                print("ok bye 👋")
-                break
-            if play == "yes":
-                flower = random.choice(list)
+name= input("Hello! What is your name? ")
+print("Nice to meet you,", name, "! Welcome to Guess the Word. 😊")
+game = True
+while game:
+    gamec = True
+    while gamec:
+        print("Would you like to play guess, 1, the animal, 2, the dessert, or 3, the flower?")
+        choice= input("Please write your choice as a 1, 2, or 3. ")
+        try:
+            choice=int(choice)
+            if choice>0 and choice<4:
+                gamec = False
+            else:
+                print("I said, give me a 1, 2, or 3.")
+        except:
+            print("Sorry, that's not even a number. Please type a 1, 2, or 3.")
+    if choice == 1:
+        os.system("cls")
+        print('*🐶🐱🐷🐰Guess the animal!!!🐸🐻🦊🐵*')
+        print("**Make sure that your answers aren't capitalized!**")
+        animallist = ["cat", "dog", "owl", "monkey", "snake", "pig", "bear", "koala", "panda", "fish", "rabbit", "horse", "bird", "turkey", "cow", "lizard"]
+        animal = random.choice(animallist)
+        animalplay = True
+        while animalplay:
+            if animal == "cat":
+                print("Hint: This animal is often kept as a pet.")
+            if animal == "dog":
+                print("Hint: This animal is man's best friend.")
+            if animal == "owl":
+                print(":Hint: This animal is a symbol of wisdom.")
+            if animal == "monkey":
+                print("Hint: This animal likes bananas.")
+            if animal == "snake":
+                print("Hint: This animal has no legs.")
+            if animal == "pig":
+                print("Hint: This animal is often pink.")
+            if animal == "bear":
+                print("Hint: Winnie the Pooh.")
+            if animal == "koala":
+                print("Hint: This animal lives in Australia.")
+            if animal == "panda":
+                print("Hint: This animal is black and white.")
+            if animal == "fish":
+                print("Hint: This animal lives in water.")
+            if animal == "rabbit":
+                print("Hint: This animal often features in cliche magic shows.")
+            if animal == "horse":
+                print("Hint: This animal can be used for transportation.")
+            if animal == "bird":
+                print("Hint: This animal is four letters.")
+            if animal ==  "turkey":
+                print("Hint: Benjamin Franklin wanted this animal to be the country's national bird.")
+            if animal == "cow":
+                print("Hint: This animal is used for milk.")
+            if animal == "lizard":
+                print("Hint: This animal is a reptile.")
+            guess = input("Your guess is...? 🤔 ")
+            if guess in animal:
+                print("🐶🐱🐷🐰 CONGRATULATIONS, " , name, "!!! You guessed correctly. 😀 🐸🐻🦊🐵")
+                aplay = input("Want to play the animal game again? 🐾 Answer yes or no. ")
+                if aplay == "no":
+                    print("ok bye 👋")
+                    animalplay = False
+                    agame = input("Want to play a different game? Answer yes or no. ")
+                    if agame == "no":
+                        game = False
+                if aplay == "yes":
+                    animal = random.choice(animallist)
+            else:
+                print("Sorry, that's not correct. 🙁")
+                aans = input("Want to guess again? Answer yes or no. ")
+                if aans == "no":
+                    print ("The animal was ", animal)
+                    aplay = input("Want to play the animal game again? 🐾 Answer yes or no. ")
+                    if aplay == "no":
+                        print("ok bye 👋")
+                        animalplay = False
+                        agame = input("Want to play a different game? Answer yes or no. ")
+                        if agame == "no":
+                            print("Bye Bye")
+                            game = False
+                    if aplay == "yes":
+                        animal = random.choice(animallist)
+    if choice == 2:
+        os.system("cls")
+        print('*🍧🍦🥧🍨Guess the dessert!!!🍫🍰🧁🍪*')
+        print("**Make sure that your answers aren't capitalized!**")
+        dessertlist = ["icecream", "cake", "pie", "candy", "chocolate", "pudding", "cheesecake", "brownie", "cobbler", "cupcake", "tiramisu"]
+        dessert = random.choice(dessertlist)
+        dessertplay = True
+        while dessertplay:
+            if dessert == "icecream":
+                print("Hint: This dessert is carried by some trucks.")
+            if dessert == "cake":
+                print("Hint: This dessert is often used to celebrate birthdays.")
+            if dessert == "pie":
+                print("Hint: 3.14.")
+            if dessert == "candy":
+                print("Hint: Don't take this dessert from strangers.")
+            if dessert == "chocolate":
+                print("Hint: This dessert is made using beans.")
+            if dessert == "pudding":
+                print("Hint: This dessert has seven letters.")
+            if dessert == "cheesecake":
+                print("Hint: This dessert has two words in it.")
+            if dessert == "brownie":
+                print("Hint: This dessert has it's color in it's name.")
+            if dessert == "cobbler":
+                print("Hint: This dessert starts with the letter c.")
+            if dessert == "cupcake":
+                print("Hint: This dessert is a smaller version of another dessert.")
+            if dessert == "tiramisu":
+                print("Hint: This dessert is coffee flavored.")
+            guess = input("Your guess is...? 🤔 ")
+            if guess in dessert:
+                print("🍧🍦🥧🍨 CONGRATULATIONS, " , name, "!!! You guessed correctly. 😀 🍫🍰🧁🍪")
+                dplay = input("Want to play the dessert game again? 🧁 Answer yes or no. ")
+                if dplay == "no":
+                    print("ok bye 👋")
+                    dessertplay = False
+                    dgame =  input("Want to play a different game? Answer yes or no. ")
+                    if dgame == "no":
+                        print("Bye Bye")
+                        game = False
+                if dplay == "yes":
+                    dessert = random.choice(dessertlist)
+            else:
+                print("Sorry, that's not correct. 🙁")
+                dans = input("Want to guess again? Answer yes or no. ")
+                if dans == "no":
+                    print ("The dessert was ", dessert)
+                    dplay = input("Want to play the dessert game again? 🧁 Answer yes or no. ")
+                    if dplay == "no":
+                        print("ok bye 👋")
+                        dessertplay = False
+                        dgame =  input("Want to play a different game? Answer yes or no. ")
+                        if dgame == "no":
+                            print("Bye Bye")
+                            game = False
+                    if dplay == "yes":
+                        dessert = random.choice(dessertlist)
+    if choice == 3:
+        os.system("cls")
+        print('*🌸🌹🌺🌻Guess the flower!!!🌼🌷🥀💐*')
+        print("**Make sure that your answers aren't capitalized!**")
+        flowerlist = ["tulip", "lilac", "sunflower", "daisy", "hibiscus", "rose", "hydrangea", "lily", "iris", "hyacinth", "daffodil", "lavendar", "poppy", "peony"]
+        flower = random.choice(flowerlist)
+        flowerplay = True
+        while flowerplay:
+            if flower == "tulip":
+                print("Hint: Netherlands is the greatest exporter of this flower.")
+            if flower == "lilac":
+                print("Hint: This flower is in the same family of the olive tree.")
+            if flower == "sunflower":
+                print("Hint: This flower has the name of a star in it's name.")
+            if flower == "daisy":
+                print("Hint: This flower symbolizes innocence.")
+            if flower == "hibiscus":
+                print("Hint: This flower thrives in warm temperatures.")
+            if flower == "rose":
+                print("Hint: This flower is the U.S. national flower.")
+            if flower == "hydrangea":
+                print("Hint: This flower is poisonous.")
+            if flower == "lily":
+                print("Hint: This flower is poisonous for cats.")
+            if flower == "iris":
+                print("Hint: This flower is also the name of the Greek goddess of rainbows and messages.")
+            if flower == "hyacinth":
+                print("Hint: In Greek mythology, Apollo turned his lover into this flower.")
+            if flower == "daffodil":
+                print("Hint: This flower is also known as narcissus.")
+            if flower == "lavendar":
+                print("Hint: This flower's scent is often used for relaxation.")
+            if flower == "poppy":
+                print("Hint: This flower is also the name of a type of seed placed on bagels.")
+            if flower == "peony":
+                print("This flower symbolizes good fortune and happy marriage.")
+            guess = input("Your guess is...? 🤔 ")
+            if guess in flower:
+                print("🌼🌷🥀💐 CONGRATULATIONS, " , name, "!!! You guessed correctly. 😀 🌸🌹🌺🌻")
+                play = input("Want to play the flower game again? 💐 Answer yes or no. ")
+                if play == "no":
+                    print("ok bye 👋")
+                    flowerplay = False
+                    fgame =  input("Want to play a different game? Answer yes or no. ")
+                    if fgame == "no":
+                        print("Bye Bye")
+                        game = False
+                if play == "yes":
+                    flower = random.choice(flowerlist)
+            else:
+                print("Sorry, that's not correct. 🙁")
+                ans = input("Want to guess again? Answer yes or no. ")
+                if ans == "no":
+                    print ("The flower was ", flower)
+                    play = input("Want to play the flower game again? 💐 Answer yes or no. ")
+                    if play == "no":
+                        print("ok bye 👋")
+                        flowerplay = False
+                        fgame =  input("Want to play a different game? Answer yes or no. ")
+                        if fgame == "no":
+                            print("Bye Bye")
+                            game = False
+                    if play == "yes":
+                        flower = random.choice(flowerlist)
